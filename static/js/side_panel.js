d4233b4961e94f0b5a7e391d2c5e03ca46c4fa0b
accaderi <127489013+accaderi@users.noreply.github.com>
2024-10-14 10:53:26 +0200

    Delete static directory

--- static/js/side_panel.js
+++
@@ -1,6 +0,0 @@
-function toggleSidePanel() {
-    const sidePanel = document.getElementById('sidePanel');
-    const mainContent = document.getElementById('mainContent');
-    sidePanel.classList.toggle('hidden');
-    mainContent.classList.toggle('expanded');
-}
